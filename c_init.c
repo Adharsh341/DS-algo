1bc4fd402b8fac529deb58291e635c3b46bc90b4
Adharsh341 <153393404+Adharsh341@users.noreply.github.com>
2025-05-24 20:05:44 +0000

    learning Macros

--- c_init.c
+++ c_init.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "stdlib.h" //using "" searches the lib files from the PWD ,, whereas using <> make it search the .h files from the /usr/include
-
+#include <string.h>
+#include<math.h>
 /*stdlib.h is a standard library header file in C that provides functions for various general-purpose operations, including:
 
 Memory management: malloc(), calloc(), realloc(), free() for dynamic memory allocation.
@@ -16,13 +17,64 @@ Terminating programs with specific exit codes
 By including stdlib.h, you can leverage these functions to write more efficient and effective C code.*/
 #include "c_init.h"
 
+// Macro definition
+#define LIMIT 5
+//chaining of Macros 
+#define INSTAGRAM FOLLOWERS
+#define FOLLOWERS 138
+
+// Multi-line Macro definition
+#define ELE 1, \
+            2, \
+            3
+// Function-like Macro definition
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+//______________________________________________________MAINCODE_________________S
 int main(){
     int a;
     a=2;
+    char s2[10] = "Geeks";
+    char s3[10] = "ForGeeks";
+    long int res;
+    // End of Declaration section  ---------------------------------------------->>>>>>>>>>
+
     printf("%d\n",a);
     add(4, 6);
     multiply(5, 5);
-    return 0;
+
+    // using math.h ---------------------------------------------->>>>>>>>>>
+    res = pow(9, 3);
+    printf("Using math.h, "
+        "The value is: %ld\n",
+        res);
+    
+    // using string.h ---------------------------------------------->>>>>>>>>>
+    strcpy(s2, s3);
+    printf("Using string.h, the strings\n I am printing Value of LIMIT  -->>%d \n "
+        "you can add double aphsotrophe in next line to continue print\n"
+        " s2 and s3: %s %s\n",
+        LIMIT,s2, s3);
+        printf("%dK insta followers\n",INSTAGRAM);
+
+    // using Macros above !!! ---------------------------------------------->>>>>>>>>>
+    int arr[] = { ELE };
+    for (int i = 0; i < 3; i++) {
+        printf("%d  \n", arr[i]);
+    }
+
+    int a1 = 18, b1 = 76;
+    printf("Minimum: %d\n", min(a1, b1));
+    
+    printf("%d\n",add_with_Macro(a1,b1));
+
+
+
+    
+
+        return 0;
 }
+
+
 // gcc c_init.c -o c_init.exe -> compiling
 //  ./c_init.exe -> run .exe
--- c_init.h
+++ c_init.h
@@ -1,3 +1,8 @@
+#ifndef C_INIT_H
+#define C_INIT_H
+#define ELE 4, 5, 6
+#define add_with_Macro(a, b) (a + b)  //macro definition
+
 void add(int a, int b)
 {
     printf("Added value=%d\n", a + b);
@@ -6,3 +11,5 @@ void multiply(int a, int b)
 {
     printf("Multiplied value=%d\n", a * b);
 }
+
+#endif  // C_INIT_H
